6ef466ad7d11b5c5c0c09bb6604e41fbbae5c60f
madsstorm <madsstorm@gmail.com>
2019-10-09 20:31:57 +0200

    Unit tests

--- test/CodeAnalyzers.Episerver.Test/Tests/AvoidUsingDataFactoryAnalyzerTests.cs
+++ test/CodeAnalyzers.Episerver.Test/Tests/AvoidUsingDataFactoryAnalyzerTests.cs
@@ -1,4 +1,5 @@
 ﻿using CodeAnalyzers.Episerver.DiagnosticAnalyzers.CSharp;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestHelper;
@@ -9,11 +10,78 @@ namespace CodeAnalyzers.Episerver.Test.Tests
     public class AvoidUsingDataFactoryAnalyzerTests : DiagnosticVerifier
     {
         [TestMethod]
-        public void CanAnalyzeEmptySource()
+        public void CanIgnoreEmptySource()
         {
-            var test = "";
+            VerifyCSharpDiagnostic("");
+        }
 
-            VerifyCSharpDiagnostic(test);
+        [TestMethod]
+        public void CanIgnoreCustomDataFactoryClass()
+        {
+            var customDataFactory = @"
+                namespace Custom
+                {
+                    public class DataFactory
+                    {
+                        public static DataFactory Instance { get; }
+                    }
+                }";
+
+            var test = @"
+                using Custom;
+
+                namespace Test
+                {
+                    public class TypeName
+                    {
+                        public void Test()
+                        {
+                            var factory = DataFactory.Instance;
+                        }
+                    }
+                }";
+
+            VerifyCSharpDiagnostic(new string[] { customDataFactory, test });
+        }
+
+        [TestMethod]
+        public void CanDetectPropertyAccess()
+        {
+            var EPiServerDataFactory = @"
+                namespace EPiServer
+                {
+                    public class DataFactory
+                    {
+                        public static DataFactory Instance { get; }
+                    }
+                }";
+
+            var test = @"
+                using EPiServer;
+
+                namespace Test
+                {
+                    public class TypeName
+                    {
+                        public void Test()
+                        {
+                            var factory = DataFactory.Instance;
+                        }
+                    }
+                }";
+
+            var expected = new DiagnosticResult
+            {
+                Id = DiagnosticIds.AvoidUsingDataFactoryAnalyzerRuleId,
+                Message = "Avoid using legacy API DataFactory.Instance",
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                   new[] {
+                            new DiagnosticResultLocation("Test1.cs", 10, 43)
+                       }
+            };
+
+            VerifyCSharpDiagnostic(new string[] { EPiServerDataFactory, test }, expected);
         }
 
         protected override DiagnosticAnalyzer GetCSharpDiagnosticAnalyzer() => new AvoidUsingDataFactoryAnalyzer();
