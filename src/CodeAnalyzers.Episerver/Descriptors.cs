8f1697977eede4810808d93386d94daf73b3f7d3
madsstorm <madsstorm@gmail.com>
2019-10-11 00:07:34 +0200

    EPI1000 descriptor

--- src/CodeAnalyzers.Episerver/Descriptors.cs
+++ src/CodeAnalyzers.Episerver/Descriptors.cs
@@ -19,11 +19,12 @@ namespace CodeAnalyzers.Episerver
         static DiagnosticDescriptor Rule(string id, string title, Category category, DiagnosticSeverity defaultSeverity, string messageFormat, string description = null)
         {
             var isEnabledByDefault = true;
-            return new DiagnosticDescriptor(id, title, messageFormat, categoryMapping.GetOrAdd(category, c => c.ToString()), defaultSeverity, isEnabledByDefault, description);
+            string helpLinkUri = "https://world.episerver.com/documentation";
+            return new DiagnosticDescriptor(id, title, messageFormat, categoryMapping.GetOrAdd(category, c => c.ToString()), defaultSeverity, isEnabledByDefault, description, helpLinkUri);
         }
 
         internal static DiagnosticDescriptor EPI1000_AvoidUsingDataFactory { get; } =
-            Rule("Epi1000", "Avoid using DataFactory", Usage, Warning,
-                "Avoid using {0}");
+            Rule("EPI1000", "Avoid using DataFactory", Usage, Warning,
+                "Avoid using {0}", "Legacy data source in EPiServer CMS. This API has been replaced by the IContentRepository, IContentEvents and a number of related interfaces.");
     }
 }
