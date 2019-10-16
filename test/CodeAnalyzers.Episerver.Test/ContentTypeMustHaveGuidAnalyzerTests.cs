8bf9ed140edfe22257eaacb9a2681345b91eab4c
Mads Storm Hansen <madsstorm@gmail.com>
2019-10-16 09:56:32 +0200

    ContentType Guid tests

--- test/CodeAnalyzers.Episerver.Test/ContentTypeMustHaveGuidAnalyzerTests.cs
+++ test/CodeAnalyzers.Episerver.Test/ContentTypeMustHaveGuidAnalyzerTests.cs
@@ -14,6 +14,27 @@ namespace CodeAnalyzers.Episerver.Test
         }
 
         [Fact]
+        public async Task CanIgnoreOtherAttribute()
+        {
+            var test = @"
+                using System;
+
+                namespace Test
+                {
+                    public class OtherAttribute : Attribute
+                    {
+                    }
+
+                    [Other]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            await Verify.VerifyAnalyzerAsync(test);
+        }
+
+        [Fact]
         public async Task CanIgnoreContentTypeWithGuid()
         {
             var test = @"
@@ -22,6 +43,27 @@ namespace CodeAnalyzers.Episerver.Test
                 namespace Test
                 {
                     [ContentType(GUID = ""1F218487-9C23-4944-A0E6-76FC1995CBF0"")]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            await Verify.VerifyAnalyzerAsync(test);
+        }
+
+        [Fact]
+        public async Task CanIgnoreCustomContentTypeWithGuid()
+        {
+            var test = @"
+                using EPiServer.DataAnnotations;
+
+                namespace Test
+                {
+                    public class CustomContentTypeAttribute : ContentTypeAttribute
+                    {
+                    }
+
+                    [CustomContentType(GUID = ""1F218487-9C23-4944-A0E6-76FC1995CBF0"")]
                     public class TypeName
                     {
                     }
@@ -50,6 +92,29 @@ namespace CodeAnalyzers.Episerver.Test
         }
 
         [Fact]
+        public async Task CanDetectCustomContentTypeWithNoArgumentList()
+        {
+            var test = @"
+                using EPiServer.DataAnnotations;
+
+                namespace Test
+                {
+                    public class CustomContentTypeAttribute : ContentTypeAttribute
+                    {
+                    }
+
+                    [CustomContentType]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            var expected = Verify.Diagnostic().WithLocation(10, 22).WithArguments("Test.TypeName");
+
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
         public async Task CanDetectContentTypeWithEmptyArgumentList()
         {
             var test = @"
@@ -64,6 +129,29 @@ namespace CodeAnalyzers.Episerver.Test
                 }";
 
             var expected = Verify.Diagnostic().WithLocation(6, 22).WithArguments("Test.TypeName");
+
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact]
+        public async Task CanDetectCustomContentTypeWithEmptyArgumentList()
+        {
+            var test = @"
+                using EPiServer.DataAnnotations;
+
+                namespace Test
+                {
+                    public class CustomContentTypeAttribute : ContentTypeAttribute
+                    {
+                    }
+
+                    [CustomContentType()]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            var expected = Verify.Diagnostic().WithLocation(10, 22).WithArguments("Test.TypeName");
 
             await Verify.VerifyAnalyzerAsync(test, expected);
         }
@@ -88,6 +176,29 @@ namespace CodeAnalyzers.Episerver.Test
         }
 
         [Fact(Skip = "TODO")]
+        public async Task CanDetectCustomContentTypeWithEmptyGuid()
+        {
+            var test = @"
+                using EPiServer.DataAnnotations;
+
+                namespace Test
+                {
+                    public class CustomContentTypeAttribute : ContentTypeAttribute
+                    {
+                    }
+
+                    [CustomContentType(GUID="")]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            var expected = Verify.Diagnostic().WithLocation(6, 22).WithArguments("Test.TypeName");
+
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact(Skip = "TODO")]
         public async Task CanDetectContentTypeWithInvalidGuid()
         {
             var test = @"
@@ -96,6 +207,29 @@ namespace CodeAnalyzers.Episerver.Test
                 namespace Test
                 {
                     [ContentType(GUID=""abc"")]
+                    public class TypeName
+                    {
+                    }
+                }";
+
+            var expected = Verify.Diagnostic().WithLocation(6, 22).WithArguments("Test.TypeName");
+
+            await Verify.VerifyAnalyzerAsync(test, expected);
+        }
+
+        [Fact(Skip = "TODO")]
+        public async Task CanDetectCustomContentTypeWithInvalidGuid()
+        {
+            var test = @"
+                using EPiServer.DataAnnotations;
+
+                namespace Test
+                {
+                    public class CustomContentTypeAttribute : ContentTypeAttribute
+                    {
+                    }
+
+                    [CustomContentType(GUID=""abc"")]
                     public class TypeName
                     {
                     }
